5d0a88edf95a5ce1a37d16e13c2e90856f23734c
João Eduardo <joaoeduardoj920@gmail.com>
2024-02-06 08:41:34 -0300

    feat: initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CadastroDeUsuario.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
